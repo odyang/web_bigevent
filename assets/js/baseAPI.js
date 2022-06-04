1e731b9c75bdcb40d437e58816319b1ddded2676
yangyang <1092434153@qq.com>
2022-06-04 21:22:22 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,20 @@
 $.ajaxPrefilter(function(options) {
     //拼接url请求地址
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // 统一为有权限的请求添加headers
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    };
+    //全局挂载complete回调函数
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message == '身份认证失败！') {
+            //   1.强制清除token
+            localStorage.removeItem('token');
+            // 2.强制退回登录页面
+            location.href = '/login.html';
+        }
+    }
+
 });
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    getUserInfo();
+    // 退出登录
+    $('#btnlogout').on('click', function() {
+        var layer = layui.layer;
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            // 1.清除本地存储的token
+            localStorage.removeItem('token');
+            // 2.回到登录页面
+            location.href = '/login.html'
+
+            layer.close(index);
+        });
+    });
+
+
+})
+
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status != 1) {
+                renderAvatar(res.data);
+            }
+        }
+    })
+};
+
+//封装渲染用户头像函数
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    //渲染用户名
+    $('.welcome').html('欢迎&nbsp&nbsp' + name);
+    //渲染用户头像
+    if (user.user_pic !== null) {
+        //渲染用户自定义头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        //渲染用户文本头像
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+        $('.layui-nav-img').hide();
+    }
+}
